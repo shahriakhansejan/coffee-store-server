d78971ef497488f8b09aaa587147de6f5e4ca574
shahriakhansejan <shahriakhansejan3@gmail.com>
2024-09-22 13:53:07 +0600

    first commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const coffeeCollection = client.db('coffeeDB').collection('coffee')
     const userCollection = client.db('coffeeDB').collection('users')
@@ -110,7 +110,7 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
